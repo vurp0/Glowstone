987c64f3597409532cda8d6c24034b1f00e786da
Max Sandholm <max@sandholm.org>
2014-09-10 22:05:58 +0300

    Added bed placement

--- src/main/java/net/glowstone/block/blocktype/BlockBed.java
+++ src/main/java/net/glowstone/block/blocktype/BlockBed.java
@@ -4,7 +4,6 @@ import net.glowstone.GlowServer;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.GlowBlockState;
 import net.glowstone.entity.GlowPlayer;
-import net.glowstone.net.message.play.game.UseBedMessage;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.ItemStack;
@@ -53,19 +52,15 @@ public class BlockBed extends BlockType {
     @Override
     public void afterPlace(GlowPlayer player, GlowBlock block, ItemStack holding) {
         if (block.getType() == Material.BED_BLOCK) {
-            BlockFace direction;
-            direction = ((Bed)block.getState().getData()).getFacing();
-            GlowBlock headBlock;
-            headBlock = block.getRelative(direction);
+            BlockFace direction = ((Bed)block.getState().getData()).getFacing();
+            GlowBlock headBlock = block.getRelative(direction);
             headBlock.setType(Material.BED_BLOCK);
-            GlowBlockState headBlockState;
-            headBlockState = headBlock.getState();
+            GlowBlockState headBlockState = headBlock.getState();
             MaterialData data = headBlockState.getData();
             ((Bed)data).setHeadOfBed(true);
             ((Bed)data).setFacingDirection(direction);
             headBlockState.setData(data);
             headBlockState.update(true);
-            player.getSession().send(new UseBedMessage(player.getEntityId(), headBlock.getX(), headBlock.getY(), headBlock.getZ()));
         }
     }
 }

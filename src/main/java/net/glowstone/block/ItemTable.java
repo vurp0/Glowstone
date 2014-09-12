01f2240e2f2184449f4d8c691d9b8e56a9ea9e1f
Gabriel Harris-Rouquette <gabizou@me.com>
2014-09-12 09:18:44 -0700

    Re-Add ItemMonsterEgg

--- src/main/java/net/glowstone/block/ItemTable.java
+++ src/main/java/net/glowstone/block/ItemTable.java
@@ -1,6 +1,7 @@
 package net.glowstone.block;
 
 import net.glowstone.block.blocktype.*;
+import net.glowstone.block.itemtype.ItemMonsterEgg;
 import net.glowstone.block.itemtype.ItemPlaceAs;
 import net.glowstone.block.itemtype.ItemSign;
 import net.glowstone.block.itemtype.ItemType;
@@ -119,6 +120,8 @@ public final class ItemTable {
         reg(Material.FLOWER_POT_ITEM, new ItemPlaceAs(Material.FLOWER_POT));
         reg(Material.SKULL_ITEM, new ItemPlaceAs(Material.SKULL));
         reg(Material.REDSTONE_COMPARATOR, new ItemPlaceAs(Material.REDSTONE_COMPARATOR_OFF));
+
+        reg(Material.MONSTER_EGG, new ItemMonsterEgg());
     }
 
     private void reg(Material material, ItemType type) {

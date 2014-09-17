a85212f8d5b258b49aad38237af247805477e828
Max Sandholm <max@sandholm.org>
2014-09-17 16:26:26 +0300

    Paintings show up on client, limit placement

--- src/main/java/net/glowstone/block/itemtype/ItemPainting.java
+++ src/main/java/net/glowstone/block/itemtype/ItemPainting.java
@@ -2,10 +2,7 @@ package net.glowstone.block.itemtype;
 
 import net.glowstone.GlowWorld;
 import net.glowstone.block.GlowBlock;
-import net.glowstone.entity.GlowEntity;
 import net.glowstone.entity.GlowPlayer;
-import net.glowstone.entity.objects.GlowPainting;
-import net.glowstone.net.message.play.entity.SpawnPaintingMessage;
 import org.bukkit.Art;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
@@ -18,20 +15,8 @@ public class ItemPainting extends ItemType {
     @Override
     public void rightClickBlock(GlowPlayer player, GlowBlock target, BlockFace face, ItemStack holding, Vector clickedLoc) {
         GlowWorld world = player.getWorld();
-        //GlowEntity paintingEntity = new GlowPainting(target.getRelative(face).getLocation(), Art.SUNSET, face);
         Painting painting = (Painting) world.spawnEntity(target.getRelative(face).getLocation(), EntityType.PAINTING);
-        painting.setArt(Art.BURNINGSKULL);
         painting.setFacingDirection(face);
-        //player.getSession().send(new SpawnPaintingMessage(paintingEntity.getEntityId(), "Sunset", paintingEntity.getLocation().getBlockX(), paintingEntity.getLocation().getBlockY(), paintingEntity.getLocation().getBlockZ(), 0));
-        /*BlockType placeAs;
-        if (face == BlockFace.UP) {
-            placeAs = ItemTable.instance().getBlock(Material.SIGN_POST);
-        } else if (face == BlockFace.DOWN) {
-            return;
-        } else {
-            placeAs = ItemTable.instance().getBlock(Material.WALL_SIGN);
-        }
-        placeAs.rightClickBlock(player, target, face, holding, clickedLoc);*/
+        painting.setArt(Art.BURNINGSKULL);
     }
-
 }
--- src/main/java/net/glowstone/entity/objects/GlowPainting.java
+++ src/main/java/net/glowstone/entity/objects/GlowPainting.java
@@ -2,9 +2,12 @@ package net.glowstone.entity.objects;
 
 import com.flowpowered.networking.Message;
 import net.glowstone.entity.GlowEntity;
-import net.glowstone.net.message.play.entity.*;
+import net.glowstone.net.message.play.entity.EntityMetadataMessage;
+import net.glowstone.net.message.play.entity.SpawnPaintingMessage;
 import org.bukkit.Art;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Painting;
@@ -22,8 +25,12 @@ public class GlowPainting extends GlowEntity implements Painting {
 
     public GlowPainting(Location location, Art art, BlockFace facingDirection) {
         super(location);
-        setArt(art);
-        this.facingDirection = facingDirection;
+        if (facingDirection == BlockFace.NORTH || facingDirection == BlockFace.SOUTH || facingDirection == BlockFace.WEST || facingDirection == BlockFace.EAST) {
+            this.facingDirection = facingDirection;
+        } else {
+            this.facingDirection = BlockFace.SOUTH;
+        }
+        this.motive = art;
     }
 
     public GlowPainting(Location location, String name, BlockFace facingDirection) {
@@ -35,7 +42,6 @@ public class GlowPainting extends GlowEntity implements Painting {
         int x = getTileX();
         int y = getTileY();
         int z = getTileZ();
-
         return Arrays.asList(new SpawnPaintingMessage(id, artToString(motive), x, y, z, getFacingInt(facingDirection)),
                 new EntityMetadataMessage(id, metadata.getEntryList()));
     }
@@ -64,7 +70,12 @@ public class GlowPainting extends GlowEntity implements Painting {
     @Override
     public boolean setArt(Art motive, boolean force) {
         this.motive = motive;
-        return true; // TODO: check the force flag, do nothing/pop it off the wall if the new motive doesn't fit
+        if (!force && !fitsOnWall(getWorld(), location, facingDirection, motive)) {
+            remove();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public int getTileX() {
@@ -118,9 +129,18 @@ public class GlowPainting extends GlowEntity implements Painting {
     }
 
     @Override
-    public boolean setFacingDirection(BlockFace blockFace, boolean b) {
-        this.facingDirection = blockFace;
-        return true; // TODO: check the force flag, do nothing/pop it off if the wall and painting direction mismatch
+    public boolean setFacingDirection(BlockFace blockFace, boolean force) {
+        if (blockFace == BlockFace.NORTH || blockFace == BlockFace.SOUTH || blockFace == BlockFace.WEST || blockFace == BlockFace.EAST) {
+            this.facingDirection = blockFace;
+        } else {
+            this.facingDirection = BlockFace.SOUTH;
+        }
+        if (!force && !fitsOnWall(getWorld(), location, facingDirection, motive)) {
+            remove();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -134,8 +154,92 @@ public class GlowPainting extends GlowEntity implements Painting {
     }
 
     @Override
+    public void pulse() {
+        super.pulse();
+        if (!fitsOnWall(getWorld(), location, facingDirection, motive)) {
+            this.remove();
+        }
+    }
+
+    @Override
     public BlockFace getFacing() {
         return facingDirection;
+    }
+
+    public static boolean fitsOnWall(World world, Location location, BlockFace facingDirection, Art motive) {
+        Block[][] blocks;
+        BlockFace right;
+        switch (facingDirection) {
+            case SOUTH:
+                right = BlockFace.EAST;
+                break;
+            case NORTH:
+                right = BlockFace.WEST;
+                break;
+            case WEST:
+                right = BlockFace.SOUTH;
+                break;
+            case EAST:
+                right = BlockFace.NORTH;
+                break;
+            default:
+                right = BlockFace.WEST;
+                break;
+        }
+        if (motive == Art.KEBAB || motive == Art.AZTEC || motive == Art.ALBAN || motive == Art.AZTEC2 || motive == Art.BOMB || motive == Art.PLANT || motive == Art.WASTELAND) {
+            blocks = new Block[1][1];
+            blocks[0][0] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace());
+        } else if (motive == Art.WANDERER || motive == Art.GRAHAM) {
+            blocks = new Block[2][1];
+            blocks[0][0] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(BlockFace.UP);
+            blocks[1][0] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace());
+        } else if (motive == Art.POOL || motive == Art.COURBET || motive == Art.SUNSET || motive == Art.SEA || motive == Art.CREEBET) {
+            blocks = new Block[1][2];
+            blocks[0][0] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace());
+            blocks[0][1] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(right);
+        } else if (motive == Art.MATCH || motive == Art.BUST || motive == Art.STAGE || motive == Art.VOID || motive == Art.SKULL_AND_ROSES || motive == Art.WITHER) {
+            blocks = new Block[2][2];
+            for (int y = 0; y < 2; y++) {
+                for (int x = 0; x < 2; x++) {
+                    blocks[y][x] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(right, x).getRelative(BlockFace.UP, y);
+                }
+            }
+        } else if (motive == Art.FIGHTERS){
+            blocks = new Block[2][4];
+            for (int y = 0; y < 2; y++) {
+                for (int x = 0; x < 4; x++) {
+                    blocks[y][x] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(right, x-1).getRelative(BlockFace.UP, y);
+                }
+            }
+        } else if (motive == Art.SKELETON || motive == Art.DONKEYKONG) {
+            blocks = new Block[3][4];
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 4; x++) {
+                    blocks[y][x] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(right, x-1).getRelative(BlockFace.UP, y-1);
+                }
+            }
+        } else if (motive == Art.POINTER || motive == Art.PIGSCENE || motive == Art.BURNINGSKULL) {
+            blocks = new Block[4][4];
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 4; x++) {
+                    blocks[y][x] = world.getBlockAt(location).getRelative(facingDirection.getOppositeFace()).getRelative(right, x-1).getRelative(BlockFace.UP, y-1);
+                }
+            }
+        } else {
+            blocks = new Block[0][0];
+        }
+
+        for (Block[] y: blocks) {
+            for (Block block: y) {
+                if (!block.getType().isSolid()) {
+                    return false;
+                }
+                if (block.getRelative(facingDirection).getType().isSolid()) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static String artToString(Art art) {
